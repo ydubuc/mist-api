2d2e17ef499f510f6f8a76acbb69caf78f965227
Y01 <69927862+ydubuc@users.noreply.github.com>
2023-03-23 14:06:15 -0400

    Merge pull request #37 from ydubuc/development
    fixes replicate input errors

--- src/media/apis/replicate/models/input_spec_sd15.rs
+++ src/media/apis/replicate/models/input_spec_sd15.rs
@@ -3,6 +3,7 @@ use serde::Serialize;
 #[derive(Debug, Serialize)]
 pub struct InputStableDiffusion15 {
     pub prompt: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub negative_prompt: Option<String>,
     pub width: u16,
     pub height: u16,
@@ -10,6 +11,8 @@ pub struct InputStableDiffusion15 {
     pub num_outputs: u8,
     pub num_inference_steps: u16,
     pub guidance_scale: u8,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub scheduler: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub seed: Option<u64>,
 }
